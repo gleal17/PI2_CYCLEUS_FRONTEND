8780d92864f369ed9618095d3dac360f95ecfd77
Guilherme Leal <guilhermelml@gmail.com>
2023-06-20 01:29:17 -0300

    feat: adicionado botões para demonstração de integração (link direto)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Typography, Button } from '@mui/material';
 
 const Home = () => {
+  const handleLock = () => {
+    fetch('http://172.20.10.14/H1:80')
+      .then(response => {
+        // Lógica de manipulação da resposta
+        console.log('Travado com sucesso!');
+      })
+      .catch(error => {
+        // Lógica de tratamento de erro
+        console.error('Erro ao travar:', error);
+      });
+  };
+
+  const handleUnlock = () => {
+    fetch('http://172.20.10.14/H2:80')
+      .then(response => {
+        // Lógica de manipulação da resposta
+        console.log('Destravado com sucesso!');
+      })
+      .catch(error => {
+        // Lógica de tratamento de erro
+        console.error('Erro ao destravar:', error);
+      });
+  };
+
   return (
     <div>
-      <h1>Bem-vindo ao Bicicletário Eletrônico!</h1>
-      <p>Este é um aplicativo para gerenciamento de bicicletário com travas por QR code.</p>
+      <Typography variant="h4" gutterBottom>Bem-vindo ao Bicicletário Eletrônico!</Typography>
+      <Typography variant="body1" gutterBottom>Este é um aplicativo para gerenciamento de bicicletário com travas por QR code.</Typography>
+      <Button variant="contained" color="primary" onClick={handleLock}>Travar</Button>
+      <Button variant="contained" color="secondary" onClick={handleUnlock}>Destravar</Button>
     </div>
   );
 };
